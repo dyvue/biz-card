b30a397fd3aab206b6fa91ceb2a0cf0ad1bd45eb
Romadhan Prasetyo <romadhanedy@gmail.com>
2021-08-22 02:51:52 +0700

    add(main): file

---
+++ main.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+console.log(`Dyvue
+Website - https://www.romadhanedy.my.id
+Twitter - https://www.twitter.com/dyvue
+Github - https://www.github.com/dyvue
+LinkedIn: https://linkedin.com/in/romadhan-prasetyo   
+Email - romadhanedy@gmail.com`);
